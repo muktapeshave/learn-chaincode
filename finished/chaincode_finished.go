03416da412cb5ef7ae873c0d25f15c6b055aedc8
muktapeshave <mukta_peshave@persistent.com>
2017-07-26 12:51:00 +0530

    modified at 26 July

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -40,7 +40,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	err := stub.PutState("key25", []byte(args[0]))
+	err := stub.PutState("key26", []byte(args[0]))
 	if err != nil {
 		return nil, err
 	}
